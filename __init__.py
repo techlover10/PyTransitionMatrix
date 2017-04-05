adf8fc90938e998c3d68f51696fa047b61491fa8
Jared <jmw535@cornell.edu>
2017-04-04 23:38:59 -0700

    test cases + init

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#
+# Copyright © 2017 jared <jared@jared-devstation>
+#
+# This file exists only to allow for use as a submodule
+
---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#
+# Copyright © 2017 jared <jared@jared-devstation>
+#
+# Test script for Markov library
+
+import Markov
+a = Markov.TransitionMatrix()
+b = Markov.TransitionMatrix()
+a.add_transition(1,2)
+a.add_transition(2,3)
+a.add_transition(3,3)
+print(a.matrix)
+b.add_transition(1,2)
+b.add_transition(2,4)
+print(b.matrix)
+a.matrix
+b.matrix
+a.save('dataA')
+b.save('dataB')
+c = Markov.TransitionMatrix('dataA.mkv')
+c.load_data('dataB.mkv')
+print(c.matrix)
+print(c.norm_matrix)
